37e6125f9d76ff49b546399bcbeeb1996f826742
natecordova <natecordovaortiz@hotmail.com>
2019-06-03 23:33:16 -0500

    commit initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+import { Employee } from './models/employee';
+
+import { FormsModule } from '@angular/forms';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +11,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'PROYECTOCRUD';
+   employeeArray: Employee[] =[
+      {id:1, name: "Bruce", country: "USA"},
+      {id:2, name: "Barry", country: "USA"},
+      {id:3, name: "Tchala", country: "Wakanda"},
+    ];
+
+    selectedEmployee: Employee = new Employee();
+
+    openForEdit(employee: Employee){
+      this.selectedEmployee = employee;
+    }
+
+    addOrEdit(){
+      
+      if(this.selectedEmployee.id === 0){
+      this.selectedEmployee.id = this.employeeArray.length + 1;
+      this.employeeArray.push(this.selectedEmployee);
+      }
+      this.selectedEmployee = new Employee();
+    }
+    
+    delete(){
+      
+      if(confirm('Are you sure you want to delete it?')){
+      this.employeeArray = this.employeeArray.filter(x => x != this.selectedEmployee);
+      this.selectedEmployee = new Employee;
+      }
+      
+    
+    }
 }
